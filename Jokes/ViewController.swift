9c0d76528141c21569f96ce48421bb8d23a7acda
Roman Yaschenkov <ryaschenkov@yandex.ru>
2025-04-18 16:26:08 +0300

    initial commit
    Initial commit for extra practicum project Jokes.

---
+++ Jokes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Jokes
+//
+//  Created by Roman Yaschenkov on 18.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
