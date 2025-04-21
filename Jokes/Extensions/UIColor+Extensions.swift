544a6bf87967c9cfe48e3a90a80c5b547291dfc7
Roman Yaschenkov <ryaschenkov@yandex.ru>
2025-04-21 10:46:45 +0300

    add: resources assets
    Add resources assets/change project folder structure.

---
+++ Jokes/Extensions/UIColor+Extensions.swift
@@ -0,0 +1,17 @@
+//
+//  UIColor+Extensions.swift
+//  Jokes
+//
+//  Created by Roman Yaschenkov on 21.04.2025.
+//
+
+import UIKit
+
+extension UIColor {
+    static var jkBackgrond: UIColor { UIColor(named: "JK Background") ?? UIColor.lightGray }
+    static var jkBlackColor: UIColor { UIColor(named: "JK Black") ?? UIColor.black }
+    static var jkLimeColor: UIColor { UIColor(named: "JK Lime") ?? UIColor.green }
+    static var jkPinkColor: UIColor { UIColor(named: "JK Pink") ?? UIColor.systemPink }
+    static var jkSplashColor: UIColor { UIColor(named: "JK Splash") ?? UIColor.green }
+    static var jkWhiteColor: UIColor { UIColor(named: "JK White") ?? UIColor.white }
+}
---
+++ Jokes/Extensions/UIFont+Extensions.swift
@@ -0,0 +1,13 @@
+//
+//  UIFont+Extensions.swift
+//  Jokes
+//
+//  Created by Roman Yaschenkov on 21.04.2025.
+//
+
+import UIKit
+
+extension UIFont {
+    static let robotoRegular16 = UIFont(name: "Roboto-Regular", size: 16)
+    static let robotoRegular24 = UIFont(name: "Roboto-Regular", size: 24)
+}
